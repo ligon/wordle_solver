a31cec104669165a990817926fd01ad411590737
Ethan Ligon <ligon@berkeley.edu>
2022-07-01 09:58:27 -0700

    Add git tracking of lists of questions and answers.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Tools to explore wordle.
 ## Autoplay
 Click here to open a Jupyter notebook that will solve today's puzzle:
  [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/ligon/wordle_solver/HEAD?labpath=autoplay.ipynb)
+ 
+ 
--- update_word_lists.py
+++ update_word_lists.py
@@ -4,6 +4,24 @@ import requests
 from bs4 import BeautifulSoup
 import bs4
 import re
+import git
+import os
+
+def commit_word_changes():
+    answerdiff = None
+    guessesdiff = None
+    repo = git.Repo(os.getcwd())
+    files = repo.git.diff(None, name_only=True).split('\n')
+    if '.answers' in files:
+        repo.git.add('.answers')
+        answerdiff = repo.git.diff('.answers')
+    if '.guesses' in files:
+        repo.git.add('.guesses')
+        guessesdiff = repo.git.diff('.guesses')
+    if '.answers' in files or '.guesses' in files:
+        repo.git.commit('-m','Update word lists.',author='ligon@berkeley.edu')
+
+    return answerdiff,guessesdiff
 
 baseurl = "https://www.nytimes.com/games/wordle/"
 page = requests.get(baseurl)
@@ -39,3 +57,14 @@ with open('.answers','w') as f:
     f.writelines("%s\n" % s for s in Answers)
     
 Guesses = Answers + Guesses
+
+adiffs,gdiffs = commit_word_changes()
+
+if adiffs is not None:
+    print("Changes in Answers!")
+    print(adiffs)
+
+
+if gdiffs is not None:
+    print("Changes in Answers!")
+    print(gdiffs)
